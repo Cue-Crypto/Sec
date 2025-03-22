090c3280e1d8458da390dd5dc81fce688e093b14
Cue-Crypto <dslifer2011@gmail.com>
2025-03-22 00:21:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -53,13 +53,3 @@ firebase.auth().onAuthStateChanged((user) => {
     googleLoginBtn.style.display = 'block';
   }
 });
-
-// Button click behavior to change iframe source
-const navButtons = document.querySelectorAll('.nav-btn');
-
-navButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const url = button.getAttribute('data-url');
-    document.getElementById('website-frame').src = url;
-  });
-});
